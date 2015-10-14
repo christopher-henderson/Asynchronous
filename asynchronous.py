1be6ea176f49dcca8c302f5d736f67b22db1b945
Christopher Henderson <chris.henderson@saifeinc.com>
2015-10-14 12:33:28 -0700

    Much work to get option decorator arguments working.

--- asynchronous.py
+++ asynchronous.py
@@ -11,34 +11,116 @@ CLS = "cls"
 
 class _Decorator(object):
 
-    def __init__(self, function):
+    def __init__(self, function=None, **kwargs):
         self.function = function
-        update_wrapper(self, function)
+        for k, v in kwargs.items():
+            self.__setattr__(k, v)
+        if function:
+            # If function was not defined then updating the wrapper
+            # is deferred until __call__ is executed.
+            update_wrapper(self, function)
 
     def __call__(self, *args, **kwargs):
-        raise NotImplementedError("Cannot call abtract class.")
+        '''
+        __call__ behaves like a dispatcher. If a function was received
+        during instantation then __child_call__ will be called immediately.
+        Otherwise __call_wrapper__ will be called
+        '''
+        if self.function:
+            # If we have a function already then that means the method
+            # signature looked like:
+            #
+            #   @my_decorator
+            #   def something():
+            #       ...
+            #
+            # ...thus we can directly call the work defined by the
+            # __child_call__. Otherwise the signature received some arguments
+            # instead of a function, a la:
+            #
+            #   @my_decorator(config=True)
+            #   def something():
+            #       ...
+            #
+            # ...in which case we need to wrap __child_call__ first via
+            # __call_wrapper__ before returning the wrapper proper.
+            return self.__child_call__(*args, **kwargs)
+        return self.__call_wrapper__(args[0])
 
     def __get__(self, obj, klass=None):
+        '''
+        Non-data descriptor for inserting an instance as the first parameter
+        to __call__ if this object is being accessed as a member.
+        '''
+        print("descriptor")
+        if obj is None:
+            raise TypeError('''\
+unbound method {NAME}() must be called with instance as first argument\
+(got nothing instead)'''.format(
+                    NAME=self.function.__name__)
+            )
         return partial(self.__call__, obj)
+
+    def __child_call__(self, *args, **kwargs):
+        '''
+        __child_call__ must be defined by inheriting classes as a surrogate
+        to __call__. That is, behavior that you would typically place under
+        __call__ should be placed under __child_call__ instread.
+        '''
+        raise NotImplementedError("Call behavior is not defined in this abstract class")
+
+    def __call_wrapper__(self, function):
+        '''
+        Updates self to wrap function.
+
+        Returns __child_call__ which defines executed behavior.
+        '''
+        self.function = function
+        update_wrapper(self, function)
+        return self
 
 
 class _AsyncBase(_Decorator):
 
-    def __call__(self, *args, **kwargs):
+    def __init__(self, function=None, daemon=True, **kwargs):
+        super(_AsyncBase, self).__init__(function)
+        self.daemon = daemon
+        self.kwargs = kwargs
+
+    def __child_call__(self, *args, **kwargs):
+        '''
+        Create, start, and return a thread-like object.
+        '''
         thread = self._THREADING_INTERFACE(
             target=self.function,
             args=args,
-            kwargs=kwargs
+            kwargs=kwargs,
+            **self.kwargs
         )
-        thread.daemon = True
+        thread.daemon = self.daemon
         thread.start()
         return thread
 
 
 class _QueuedResultBase(_AsyncBase):
 
-    def __init__(self, function):
-        super(_QueuedResultBase, self).__init__(function)
+    def __init__(self, function=None, daemon=True, **kwargs):
+        super(_QueuedResultBase, self).__init__(function, daemon, **kwargs)
+        if self.function:
+            self._get_insertion_index()
+        else:
+            self.QUEUE_INSERTION_INDEX = -1
+
+    def __child_call__(self, *args, **kwargs):
+        if self.QUEUE_INSERTION_INDEX is -1:
+            # Definition of self.function was deferred until now.
+            self._get_insertion_index()
+        queue = Queue()
+        args = list(args)
+        args.insert(self.QUEUE_INSERTION_INDEX, queue)
+        return super(_QueuedResultBase, self).__child_call__(*args, **kwargs), queue
+
+    def _get_insertion_index(self):
         # If we are decorating an unbound method then our expectation is
         # that our queueing object will be inserted as the first argument.
         #
@@ -57,18 +139,12 @@ class _QueuedResultBase(_AsyncBase):
         # intent (e.g. I can check to see if the first argument given has the
         # target function as a member, but what if we are decorating something
         # like a copy constructor...?).
-        argspec = getargspec(function)[0]
+        argspec = getargspec(self.function)[0]
         isMethodOrClassmethod = (
             argspec and
             (argspec[0] == SELF or argspec[0] == CLS)
         )
         self.QUEUE_INSERTION_INDEX = 1 if isMethodOrClassmethod else 0
-
-    def __call__(self, *args, **kwargs):
-        queue = Queue()
-        args = list(args)
-        args.insert(self.QUEUE_INSERTION_INDEX, queue)
-        return super(_QueuedResultBase, self).__call__(*args, **kwargs), queue
 
 
 class _BlockingBase(_QueuedResultBase):
